2674eeb9e512b926ccee0ec563a16ebd8f8716d2
priscilla5328 <21400032@handong.edu>
2018-05-28 05:50:45 +0900

    week 13(2)

--- mysource/filesimple.c
+++ mysource/filesimple.c
@@ -54,9 +54,9 @@ void jsonNameList(char* jsonstr, jsmntok_t *t, int tokcount, int *nameTokIndex){
   int a=0, b=0, count=0;
 
   for (a=0; a<tokcount; a++){
+		//printf("%d ", t[a].size);
     if((t[a].size == 1) && (t[a].type != JSMN_OBJECT) && (t[a].type != JSMN_ARRAY)){
       nameTokIndex[b]=a;
-      //printf("a:%d nameTokIndex[b]:%d\n", a, nameTokIndex[b]);
 
       count++;
       b++;
@@ -67,24 +67,41 @@ void jsonNameList(char* jsonstr, jsmntok_t *t, int tokcount, int *nameTokIndex){
 
 }
 
+void printObjectList(char* jsonstr, jsmntok_t *t, int tokcount, int *nameTokIndex){
+	printf("***** Object List *****\n");
+
+	int a=0, b=0;
+
+  for (a=0; a<tokcount; a++){
+    if(t[a].size == 8){
+      nameTokIndex[b]=a;
+
+			printf("object[%d] %.*s\n", b+1, t[a+2].end-t[a+2].start, jsonstr+t[a+2].start);
+			//printf("%d %d %d\n",   nameTokIndex[0],  nameTokIndex[1],  nameTokIndex[2]);
+			b++;
+    }
+    nameTokIndex[b]=a+1;
+    //printf("a:%d nameTokIndex[b]:%d\n", a+1, nameTokIndex[b]);
+  }
+}
+
 void printNameList(char* jsonstr, jsmntok_t *t, int *nameTokIndex){
   int i=0;
 
   printf("***** Name List *****\n");
   while(1){
-    if(nameTokIndex[i] == 0){
+    if((nameTokIndex[i] == 0) || (t[nameTokIndex[i]].start == 0)){
       break;
-    }
-    else if(nameTokIndex[i] != 0){
+    }else if(nameTokIndex[i] != 0){
       printf("name[%d] %.*s\n", i+1, t[nameTokIndex[i]].end - t[nameTokIndex[i]].start, jsonstr + t[nameTokIndex[i]].start);
       //printf("%d %d\n", jsonstr + t[nameTokIndex[i]].start, jsonstr);
       //printf("%.*s\n", t[nameTokIndex[i+1]-1].end-t[nameTokIndex[i+1]-1].start, jsonstr+t[nameTokIndex[i+1]-1].start);
       i++;
-      if(jsonstr + t[nameTokIndex[i]].start == jsonstr){
-        break;
-      }
+			//printf("end %d    start %d\n", t[nameTokIndex[i]].end, t[nameTokIndex[i]].start);
+
     }
   }
+
 }
 
 void selectNameList(char* jsonstr, jsmntok_t *t, int* nameTokIndex){
@@ -97,14 +114,40 @@ void selectNameList(char* jsonstr, jsmntok_t *t, int* nameTokIndex){
     if(no == 0){
       break;
     }
-    else if((nameTokIndex[no-1] != 0) && (nameTokIndex[no] != 0)){
-      //printf("%d %d\n", nameTokIndex[no]-1, nameTokIndex[no]);
+    else if((nameTokIndex[no-1] != 0) ){
+      //printf("%d \n", nameTokIndex[no]-1);
 
       printf("name[%d] %.*s\n", no, t[nameTokIndex[no-1]].end-t[nameTokIndex[no-1]].start, jsonstr+t[nameTokIndex[no-1]].start);
       printf("%.*s\n", t[nameTokIndex[no]-1].end-t[nameTokIndex[no]-1].start, jsonstr+t[nameTokIndex[no]-1].start);
       //printf("%.*s\n", t[21].end-t[21].start, jsonstr+t[21].start);
     }
   }
+}
+
+void selectObjectList(char* jsonstr, jsmntok_t *t, int* nameTokIndex, char* obj){
+	int no, n;
+
+	obj = (char *)malloc(sizeof(char));
+
+
+	while(1){
+		printf("Select Object's NO (exit:0) >> ");
+		scanf("%d", &no);
+		n = strlen(obj);
+		printf("n0:%d\n", n);//410
+
+		if(no == 0 || (t[nameTokIndex[no-1]].end < t[nameTokIndex[no-1]].start)){
+			break;
+		}else if(nameTokIndex[no-1] != 0){
+
+			obj = (char *)realloc(obj, t[nameTokIndex[no-1]].end-t[nameTokIndex[no-1]].start);
+			strncpy(obj, jsonstr+t[nameTokIndex[no-1]].start, t[nameTokIndex[no-1]].end-t[nameTokIndex[no-1]].start);
+
+			printf("%.*s\n", t[nameTokIndex[no-1]].end-t[nameTokIndex[no-1]].start, obj);
+			//printf("%d\n", t[nameTokIndex[no-1]].start);
+			//printf("object[%d] %.*s\n", no, t[nameTokIndex[no-1]].end-t[nameTokIndex[no-1]].start, jsonstr+t[nameTokIndex[no-1]].start);
+		}
+	}
 }
 
 static int jsoneq(const char *json, jsmntok_t *tok, const char *s) {
@@ -121,7 +164,8 @@ int main() {
   int count;
   int nameTokIndex[100]={0,};
 	jsmn_parser p;
-	jsmntok_t t[128]; /* We expect no more than 128 tokens */
+	jsmntok_t t[128];
+	char* obj;/* We expect no more than 128 tokens */
 
 	char* JSON_STRING;
 	JSON_STRING = readJSONFile();
@@ -136,11 +180,13 @@ int main() {
 
 
 	/* Assume the top-level element is an object */
-	if (r < 1 || t[0].type != JSMN_OBJECT) {
-		printf("Object expected\n");
+	if (r < 1 || t[0].type != JSMN_ARRAY) {
+		printf("Array expected\n");
 		return 1;
 	}
 
+	printObjectList(JSON_STRING, t, r, nameTokIndex);
+	selectObjectList(JSON_STRING, t, nameTokIndex, obj);
   jsonNameList(JSON_STRING, t, r, nameTokIndex);
   printNameList(JSON_STRING, t, nameTokIndex);
   selectNameList(JSON_STRING, t, nameTokIndex);
